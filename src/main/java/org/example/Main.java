8705602be707bc8f452f9dd8ee2d48eed6b2f217
Stéfano de Faria <stefano.defaria-ae@transvoyant.com>
2024-03-20 14:15:49 -0300

    add gzip cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,29 @@
 package org.example;
 
-import org.apache.tomcat.util.http.parser.Authorization;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.zalando.logbook.Correlation;
-import org.zalando.logbook.HeaderFilter;
-import org.zalando.logbook.HttpHeaders;
-import org.zalando.logbook.HttpRequest;
+import org.zalando.logbook.BodyReplacer;
 import org.zalando.logbook.HttpResponse;
 import org.zalando.logbook.Logbook;
-import org.zalando.logbook.Precorrelation;
-import org.zalando.logbook.Sink;
-import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.BodyFilters;
 import org.zalando.logbook.core.DefaultHttpLogWriter;
 import org.zalando.logbook.core.DefaultSink;
 import org.zalando.logbook.core.HeaderFilters;
+import org.zalando.logbook.core.ResponseFilters;
 import org.zalando.logbook.json.JsonHttpLogFormatter;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.function.Predicate;
+import java.util.zip.GZIPInputStream;
+
+import javax.annotation.Nullable;
+
+import static org.springframework.http.HttpHeaders.CONTENT_ENCODING;
+import static org.zalando.logbook.core.Conditions.exclude;
+import static org.zalando.logbook.core.Conditions.requestTo;
 
 @SpringBootApplication
 public class Main {
@@ -32,9 +36,47 @@ public class Main {
     @Bean
     public Logbook logbook() {
         return Logbook.builder()
+                      .condition(exclude(
+                              requestTo("/**/actuator/**"),
+                              requestTo("/not-found")
+                      ))
                       .sink(new DefaultSink(new JsonHttpLogFormatter(), new DefaultHttpLogWriter()))
+                      .responseFilter(ResponseFilters.replaceBody(new UnzipBodyReplacer()))
+                      .bodyFilter(BodyFilters.truncate(4096))
+                      .headerFilter(HeaderFilters.defaultValue())
                       .headerFilter(HeaderFilters.replaceCookies("JSESSIONID"::equalsIgnoreCase, "XXX"))
                       .headerFilter(HeaderFilters.replaceCookies("__Secure-Request-Token"::equalsIgnoreCase, "XXX"))
                       .build();
     }
+
+    @Slf4j
+    static class UnzipBodyReplacer implements BodyReplacer<HttpResponse> {
+
+        @Nullable
+        @Override
+        public String replace(HttpResponse httpResponse) {
+            if ("gzip".equalsIgnoreCase(httpResponse.getHeaders().getFirst(CONTENT_ENCODING))) {
+                try (GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(httpResponse.getBody()));
+                     ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = gis.read(buffer)) > 0) {
+                        outputStream.write(buffer, 0, len);
+                    }
+                    return outputStream.toString();
+                } catch (IOException e) {
+                    log.error("Error decompressing body", e);
+                }
+            }
+
+            try {
+                return httpResponse.getBodyAsString();
+            } catch (IOException e) {
+                log.error("Error extracting non-zipped body", e);
+                return "<UnzipBodyReplacer error>";
+            }
+        }
+    }
+
 }
--- src/main/java/org/example/SampleController.java
+++ src/main/java/org/example/SampleController.java
@@ -1,21 +1,85 @@
 package org.example;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @RestController
 public class SampleController {
 
-    @PostMapping(path = "/test/**", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> sampleEndpoint() {
+    @Value("classpath:test-gzipped-file.gz")
+    Resource gzippedFile;
+
+    private byte[] gzippedBytes;
+
+    @PostConstruct
+    public void init() throws IOException {
+        gzippedBytes = new byte[(int) gzippedFile.getFile().length()];
+        try (FileInputStream inputStream = new FileInputStream(gzippedFile.getFile())) {
+            inputStream.read(gzippedBytes);
+        }
+    }
+
+    @GetMapping(path = "/any/**", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> any() {
 
         return ResponseEntity.status(HttpStatus.OK)
                              .header("X-CustomHeader", "CustomHeaderValue")
                              .body("{\"data\": \"ResponseData1234\"}");
+    }
+
+    @PostMapping(path = "/uncompressed-ok", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uncompressedEndpoint() {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .header("X-CustomHeader", "CustomHeaderValue")
+                             .body("{\"data\": \"ResponseData1234\"}");
+    }
+
+    @PostMapping(path = "/uncompressed-bad-request", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uncompressedBadRequestEndpoint() {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .header("X-CustomHeader", "CustomHeaderValue")
+                             .body("{\"data\": \"ResponseData1234\"}");
+    }
+
+    @GetMapping(path = "/gzipped-ok", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<byte[]> gzippedExample() {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .header("content-encoding", "gzip")
+                             .body(gzippedBytes);
+
+    }
+
+    @GetMapping(path = "/gzipped-bad-request", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<byte[]> gzippedBadRequestExample() {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .header("content-encoding", "gzip")
+                             .body(gzippedBytes);
+
+    }
+
+    @GetMapping(path = "/gzipped-fake", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<byte[]> gzippedFakeExample() {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .header("content-encoding", "gzip")
+                             .body("not gzipped!".getBytes());
+
     }
 }
